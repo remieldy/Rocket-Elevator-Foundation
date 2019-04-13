b0bba92b3a81a94c85cc3ad67552a242a29b982a
[remieldy].github.io <46905440+remieldy@users.noreply.github.com>
2019-04-13 00:01:48 -0400

    none done

--- app/assets/javascripts/intervention.js
+++ app/assets/javascripts/intervention.js
@@ -23,7 +23,7 @@ $(document).ready(function(){
 $.get( "interventions/get_buildings_from_customer", {customer_id: customerId}, function( data ) {
   $("#building_id").show();  
   $("#building_id").empty();
-  	$("#building_id").append('<option>Select Building</option>');
+  	$("#building_id").append('<option>NONE</option>');
   	for(var i = 0; i < data.length; i++){
   		$("#building_id").append('<option value="' + data[i]["id"] + '">' +data[i]["id"] + '</option>');
 	      	}
@@ -38,7 +38,7 @@ $("#building_id").on('change', function(){
   $.get( "interventions/get_battery_from_building", {building_id: buildingId}, function( data ) {
     $("#battery_id").show();
     $("#battery_id").empty();
-    $("#battery_id").append('<option>Select battery</option>');
+    $("#battery_id").append('<option>NONE</option>');
     for(var i = 0; i < data.length; i++){
       $("#battery_id").append('<option value="' + data[i]["id"] + '">' +data[i]["id"] + '</option>');
     }
@@ -53,7 +53,7 @@ $("#battery_id").on('change', function(){
   $.get( "interventions/get_column_from_battery", {battery_id: batteryId}, function( data ) {
     $("#column_id").show();
     $("#column_id").empty();
-    $("#column_id").append('<option>Select Column</option>');
+    $("#column_id").append('<option>NONE</option>');
     for(var i = 0; i < data.length; i++){
       $("#column_id").append('<option value="' + data[i]["id"] + '">' +data[i]["id"] + '</option>');
     }
@@ -68,7 +68,7 @@ $("#column_id").on('change', function(){
   $.get( "interventions/get_elevator_from_column", {column_id: columnId}, function( data ) {
     $("#elevator_id").show();
     $("#elevator_id").empty();
-    $("#elevator_id").append('<option>Select elevator</option>');
+    $("#elevator_id").append('<option>NONE</option>');
     for(var i = 0; i < data.length; i++){
       $("#elevator_id").append('<option value="' + data[i]["id"] + '">' +data[i]["id"] + '</option>');
     }
